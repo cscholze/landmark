830cb41fd5a1dc2c7cd0f9b0d33effbe8dd04f2a
Caleb Scholze <caleb.scholze@gmail.com>
2021-01-04 22:41:03 -0600

    Completed "Handling User Input"
    Completed the SwiftUI tutorial up through https://developer.apple.com/tutorials/swiftui/handling-user-input

---
+++ Landmark/Views/Landmarks/LandmarkList.swift
@@ -0,0 +1,42 @@
+//
+//  LandmarksList.swift
+//  Landmark
+//
+//  Created by Caleb Scholze on 1/4/21.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    @EnvironmentObject var modelData: ModelData
+    @State private var showFavoritesOnly = false
+    
+    var filteredLandmarks: [Landmark] {
+        modelData.landmarks.filter { landmark in
+            (!showFavoritesOnly || landmark.isFavorite)
+        }
+    }
+    
+    var body: some View {
+        NavigationView {
+            List {
+                Toggle(isOn: $showFavoritesOnly) {
+                    Text("Favorites Only")
+                }
+                ForEach(filteredLandmarks) { landmark in
+                NavigationLink(destination: LandmarkDetail(landmark: landmark)) {
+                        LandmarkRow(landmark: landmark)
+                    }
+                }
+            }
+            .navigationTitle("Landmarks")
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkList()
+            .environmentObject(ModelData())
+    }
+}
