3769f289755e472a9c019be2037cfc00decf5480
Caleb Scholze <caleb.scholze@gmail.com>
2021-01-05 22:42:34 -0600

    Completed "Drawing Paths and Shapes"
    https://developer.apple.com/tutorials/swiftui/drawing-paths-and-shapes

---
+++ Landmark/Views/Badges/Badges/RotateBadgeSymbol.swift
@@ -0,0 +1,24 @@
+//
+//  RotateBadgeSymbol.swift
+//  Landmark
+//
+//  Created by Caleb Scholze on 1/5/21.
+//
+
+import SwiftUI
+
+struct RotateBadgeSymbol: View {
+    let angle: Angle
+    
+    var body: some View {
+        BadgeSymbol()
+            .padding(-60)
+            .rotationEffect(angle, anchor: .bottom)
+    }
+}
+
+struct RotateBadgeSymbol_Previews: PreviewProvider {
+    static var previews: some View {
+        RotateBadgeSymbol(angle: Angle(degrees: 5))
+    }
+}
